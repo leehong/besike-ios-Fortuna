ac6ae2e4f67f0952b0d6a8cf1bb45f9ca4352b97
stackvirgil <virgil.leehong@gmail.com>
2014-10-17 21:12:11 +0800

    init

--- besike-ios-Fortuna/AppDelegate.swift
+++ besike-ios-Fortuna/AppDelegate.swift
@@ -12,10 +12,23 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var positiveQuotes: [String]!
+    var negativeQuotes: [String]!
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let positivePath =
+            NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")
+        let negativePath =
+            NSBundle.mainBundle().pathForResource("negativeQuotes", ofType: "json")
+        
+        positiveQuotes = loadJSON(positivePath!) as [String]!
+        negativeQuotes = loadJSON(negativePath!) as [String]!
+        
+        assert(positiveQuotes.count > 0, "should load positive quotes")
+        assert(negativeQuotes.count > 0, "should load negative quotes")
+
         return true
     }
 
@@ -39,6 +52,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func loadJSON(path: String) -> AnyObject? {
+        let data = NSData(contentsOfFile: path)
+        assert(data != nil, "Failed to read data from: \(path)")
+        
+        // Parse JSON data
+        var err : NSError?
+        let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: &err)
+        assert(err == nil, "Error parsing json: \(err)")
+        
+        return json
     }
 
 
--- besike-ios-Fortuna/ViewController.swift
+++ besike-ios-Fortuna/ViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quotationTextView: UITextView!
+    
+    @IBAction func rootViewTapped(sender: UITapGestureRecognizer) {
+        displayRandomQuote()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayRandomQuote()
         // Do any additional setup after loading the view, typically from a nib.
+        println("Hacking in Swift, since iOS" + "8")
+        println("controller: \(self)")
+        println("view: \(self.view)")
+        println("view's frame: \(self.view.frame)")
+        let path = NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")
+        println("positive quotes path: \(path)")
+        
+        println("viewDidLoad quotationTextView: \(quotationTextView)")
+        
+        quotationTextView.editable = false
+        quotationTextView.selectable = false
+        displayRandomQuote()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func awakeFromNib() {
+        // The super method doesn't do anything, but we call it anyway.
+        super.awakeFromNib()
+        println("awakeFromNib quotationTextView: \(quotationTextView)")
+    }
+    
+    func displayRandomQuote() {
+        
+        var delegate = UIApplication.sharedApplication().delegate as AppDelegate
+        var quotes : [String]!
+        quotes = arc4random_uniform(2) == 1 ? delegate.positiveQuotes : delegate.negativeQuotes
+        var quote : String = quotes[Int(arc4random_uniform(UInt32(quotes.count)))]
+        quotationTextView.text = quote
     }
 
 
