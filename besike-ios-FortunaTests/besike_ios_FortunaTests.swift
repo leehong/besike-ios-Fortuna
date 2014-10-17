ed17a05db2bacc8bad0db85cf3470ccb78dcdb90
stackvirgil <virgil.leehong@gmail.com>
2014-10-17 21:00:17 +0800

    Initial Commit

---
+++ besike-ios-FortunaTests/besike_ios_FortunaTests.swift
@@ -0,0 +1,36 @@
+//
+//  besike_ios_FortunaTests.swift
+//  besike-ios-FortunaTests
+//
+//  Created by stackvirgil on 14-10-17.
+//  Copyright (c) 2014年 stackvirgil. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class besike_ios_FortunaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
